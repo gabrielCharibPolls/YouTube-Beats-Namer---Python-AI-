62bc01427154b3ebf77a447738617a86ed59bd4c
Gabriel Charib <110329448+gabrielCharibPolls@users.noreply.github.com>
2023-10-01 02:41:10 +0200

    correct information extraction and filtering

--- recupe_data_youtube.py
+++ recupe_data_youtube.py
@@ -1,35 +1,33 @@
-import csv
 import requests
-from bs4 import BeautifulSoup
+import re
+import csv
 
-# URL de la recherche YouTube
 url = "https://www.youtube.com/results"
 params = {
     "search_query": "Free Bombap Beats"
 }
 
-# Effectuer la requête HTTP
 response = requests.get(url, params=params)
 
-# Vérifier si la requête a réussi
 if response.status_code == 200:
-    # Analyser le contenu de la page avec BeautifulSoup
-    soup = BeautifulSoup(response.text, "html.parser")
-    
-    # Extraire les titres des vidéos
-    titles = [a.get_text() for a in soup.find_all("a", class_="yt-simple-endpoint style-scope ytd-video-renderer")]
-    print('titles')
-    # Filtrer les titres commençant par "Free Bombap Beats"
-    bombap_titles = [title for title in titles if title.startswith("Free Bombap Beats")]
+    content = response.text
+    #################################################################
+    # Utiliser des expressions régulières pour extraire les informations
+    titles = re.findall(r'\"text\":\"(\(FREE\) .*?)\"', content)
 
-    # Enregistrer les titres dans un fichier CSV
-    with open('bombap_beats_titles.csv', 'w', newline='', encoding='utf-8') as csv_file:
-        csv_writer = csv.writer(csv_file)
-        csv_writer.writerow(['Titre de la vidéo'])
 
-        for title in bombap_titles:
-            csv_writer.writerow([title])
+    with open('youtube_titles.csv', 'w', newline='', encoding='utf-8') as csvfile:
+        writer = csv.writer(csvfile)
+        writer.writerow(["Video Number", "Title", "URL", "Video ID"])  # Écrire les en-têtes
 
-    print("Les titres ont été enregistrés avec succès dans 'bombap_beats_titles.csv'.")
+        for i in range(min(50, len(titles))):
+            title = titles[i]
+            print(f"Video {i+1}:")
+            print("Titre:", title)
+            print("-" * 50)
+
+            # Écrire les données dans le fichier CSV
+            writer.writerow([i+1, title])
+
 else:
     print("La requête a échoué avec le code :", response.status_code)
