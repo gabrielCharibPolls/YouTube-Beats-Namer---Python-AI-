e3138e13692597e512f9b41fea035b974478057e
Gabriel Charib <110329448+gabrielCharibPolls@users.noreply.github.com>
2023-10-01 02:52:52 +0200

    repair csv file savers
    todo:
    
    "Clean up the CSV file because sometimes, in the presence of '\', we don't get the full title."

--- recupe_data_youtube.py
+++ recupe_data_youtube.py
@@ -1,6 +1,7 @@
 import requests
 import re
 import csv
+import os
 
 url = "https://www.youtube.com/results"
 params = {
@@ -15,12 +16,18 @@ if response.status_code == 200:
     # Utiliser des expressions régulières pour extraire les informations
     titles = re.findall(r'\"text\":\"(\(FREE\) .*?)\"', content)
 
+    # Vérifier si le fichier existe déjà
+    file_exists = os.path.isfile('youtube_titles.csv')
 
-    with open('youtube_titles.csv', 'w', newline='', encoding='utf-8') as csvfile:
+    # Ouvrir un fichier CSV en mode append
+    with open('youtube_titles.csv', 'a', newline='', encoding='utf-8') as csvfile:
         writer = csv.writer(csvfile)
-        writer.writerow(["Video Number", "Title", "URL", "Video ID"])  # Écrire les en-têtes
+        
+        # Si le fichier n'existait pas, écrire les en-têtes
+        if not file_exists:
+            writer.writerow(["Video Number", "Title"])  # Écrire les en-têtes
 
-        for i in range(min(50, len(titles))):
+        for i in range(min(70, len(titles))):
             title = titles[i]
             print(f"Video {i+1}:")
             print("Titre:", title)
